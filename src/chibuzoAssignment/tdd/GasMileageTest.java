07eaddc77f8b47667ae60c170fdb2380c612aaa9
Philip Odey <philipodey75@gmail.com>
2023-09-13 02:20:58 +0100

    iudu

--- src/chibuzoAssignment/tdd/GasMileageTest.java
+++ src/chibuzoAssignment/tdd/GasMileageTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class GasMileageTest {
     @Test
     public void testMilesDrivenPerGallon(){
-        double expected = 10.0;
+        double expected = 10.0;git
         GasMileage toyota = new GasMileage();
         double actual = toyota.milesPerGallon(100, 10);
         assertEquals(expected, actual, 0.01);
