1cc22a6b9be4b6e8d74be6fef897740bc3e01cb1
Samuel Shedrach <shedrachsamuel4@gmail.com>
2023-09-16 14:05:59 +0100

    committed

--- src/Functions.java
+++ src/Functions.java
@@ -1,5 +1,9 @@
 public class Functions {
     public static void main(String[] args) {
 
-
-    }}
+    }
+    public static void great(String x){
+      x= "25";
+        System.out.printf("%s", x);
+        }
+}
